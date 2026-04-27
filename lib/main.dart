a18a56e5e2fe1ee439133c01bb52bb2a9e6143a2
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-04-27 21:47:57 +0700

    perubahan di main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-import 'providers/auth_provider.dart';
+import 'firebase_options.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Inisialisasi Firebase menghubungkan aplikasi ke project 'verifikasi-email'
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Merchandise48',
+            title: 'Merchandise48',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Home')),
-        body: const Center(child: Text('App Ready 🚀')),
+        body: Center(
+          child: Text(
+            'welcome to Merchandise48!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
