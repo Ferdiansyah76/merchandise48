e2c02e93ddd2328ed721952ec04d3b58eac0e426
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-04-30 19:05:56 +0700

    penambahan di login page

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:email_validator/email_validator.dart';
 import '../../../../../features/auth/presentation/widgets/custom_text_field.dart';
 import '../../../../../features/auth/presentation/widgets/custom_button.dart';
