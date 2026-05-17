beaebb6daba6356eb45b922a612e069aafa31b65
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2026-05-17 21:54:07 +0700

    pembaruan pada kodingan eror 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'core/routes/app_router.dart';
+import 'package:merchandise48/core/theme/app_theme.dart';
+import 'package:merchandise48/features/auth/presentation/providers/auth_provider.dart';
+import 'package:merchandise48/features/dashboard/presentation/providers/product_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Inisialisasi Firebase menghubungkan aplikasi ke project 'verifikasi-email'
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-    runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-            title: 'Merchandise48',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'welcome to Merchandise48!',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: MaterialApp(
+        title:                     'My App',
+        debugShowCheckedModeBanner: false,
+        theme:                     AppTheme.light,
+        initialRoute:              AppRouter.splash,
+        routes:                    AppRouter.routes,
       ),
     );
   }
